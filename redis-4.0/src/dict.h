d2531f758f607919531d7ccf968c75b52de72e82
hoohack <hhqcontinue@gmail.com>
2017-11-25 17:05:58 +0800

    read little dict.h

--- redis-4.0/src/dict.h
+++ redis-4.0/src/dict.h
@@ -44,54 +44,60 @@
 /* Unused arguments generate annoying warnings... */
 #define DICT_NOTUSED(V) ((void) V)
 
+/* 哈希表节点 */
 typedef struct dictEntry {
-    void *key;
+    void *key; /* 键名 */
     union {
         void *val;
         uint64_t u64;
         int64_t s64;
         double d;
-    } v;
-    struct dictEntry *next;
+    } v; /* 值 */
+    struct dictEntry *next; /* 指向下一个节点, 将多个哈希值相同的键值对连接起来*/
 } dictEntry;
 
+/* 保存一连串操作特定类型键值对的函数 */
 typedef struct dictType {
-    uint64_t (*hashFunction)(const void *key);
-    void *(*keyDup)(void *privdata, const void *key);
-    void *(*valDup)(void *privdata, const void *obj);
-    int (*keyCompare)(void *privdata, const void *key1, const void *key2);
-    void (*keyDestructor)(void *privdata, void *key);
-    void (*valDestructor)(void *privdata, void *obj);
+    uint64_t (*hashFunction)(const void *key); /* 哈希函数 */
+    void *(*keyDup)(void *privdata, const void *key); /* 复制键函数 */
+    void *(*valDup)(void *privdata, const void *obj); /* 复制值函数 */
+    int (*keyCompare)(void *privdata, const void *key1, const void *key2); /* 比较键函数 */
+    void (*keyDestructor)(void *privdata, void *key); /* 销毁键函数 */
+    void (*valDestructor)(void *privdata, void *obj); /* 销毁值函数 */
 } dictType;
 
 /* This is our hash table structure. Every dictionary has two of this as we
  * implement incremental rehashing, for the old to the new table. */
+/* 哈希表结构 */
 typedef struct dictht {
-    dictEntry **table;
-    unsigned long size;
-    unsigned long sizemask;
-    unsigned long used;
+    dictEntry **table; /* 哈希表节点数组 */
+    unsigned long size; /* 哈希表大小 */
+    unsigned long sizemask; /* 哈希表大小掩码，用于计算哈希表的索引值，大小总是dictht.size - 1 */
+    unsigned long used; /* 哈希表已经使用的节点数量 */
 } dictht;
 
+/* 字典结构 每个字典有两个哈希表，需要用在将旧表rehash到新表 */
 typedef struct dict {
-    dictType *type;
-    void *privdata;
-    dictht ht[2];
-    long rehashidx; /* rehashing not in progress if rehashidx == -1 */
-    unsigned long iterators; /* number of iterators currently running */
+    dictType *type; /* 类型特定函数 */
+    void *privdata; /* 保存类型特定函数需要使用的参数 */
+    dictht ht[2]; /* 保存的两个哈希表，ht[0]是真正使用的，ht[1]会在rehash时使用 */
+    long rehashidx; /* rehashing not in progress if rehashidx == -1 rehash进度，如果不等于-1，说明还在进行rehash */
+    unsigned long iterators; /* number of iterators currently running 正在运行中的遍历器数量 */
 } dict;
 
 /* If safe is set to 1 this is a safe iterator, that means, you can call
  * dictAdd, dictFind, and other functions against the dictionary even while
  * iterating. Otherwise it is a non safe iterator, and only dictNext()
  * should be called while iterating. */
+/* 如果safe属性等于1，说明这是一个安全的遍历器，就是说，你可以在遍历的同时调用dictAdd, dictFind以及其他函数,
+ * 否则就不是安全的遍历器函数，在进行遍历时只能调用dichNext函数 */
 typedef struct dictIterator {
-    dict *d;
-    long index;
+    dict *d; /* 操作的字典 */
+    long index; /* 下标 */
     int table, safe;
-    dictEntry *entry, *nextEntry;
+    dictEntry *entry, *nextEntry; /* 当前哈希表节点及下一个哈希表节点 */
     /* unsafe iterator fingerprint for misuse detection. */
-    long long fingerprint;
+    long long fingerprint; /* 当我们初始化一个不安全的迭代器时，就会得到字典的finigerprint(指纹)，当迭代器释放时，我们会再次检查指纹。如果两次的指纹不一致，那么意味着不安全迭代器的调用者在迭代过程中对字典进行了非法的调用。*/
 } dictIterator;
 
 typedef void (dictScanFunction)(void *privdata, const dictEntry *de);
